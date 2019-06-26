bd14c6768dce45381a9cca2d33488e463be25b52
Vitas1968 <dor1968@mail.ru>
2019-06-26 20:59:18 +0700

    Update and rename src/Main.java to  src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,5 @@ public class Main
     {
         System.out.println("Hello World!!!");
         System.out.println();
-        System.out.println();
-        System.out.println("Да пошди вы!");
-
     }
 }
